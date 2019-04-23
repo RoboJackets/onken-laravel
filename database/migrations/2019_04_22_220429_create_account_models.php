0dd7df30411b4bb769fbc78c0637e289ffd88896
Josh Oldenburg <josh.oldenburg@me.com>
2019-04-22 22:38:50 -0400

    Fix migration

--- database/migrations/2019_04_22_220429_create_account_models.php
+++ database/migrations/2019_04_22_220429_create_account_models.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModels extends Migration
+class CreateAccountModels extends Migration
 {
     /**
      * Run the migrations.
