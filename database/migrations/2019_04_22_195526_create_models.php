7ed3efdb7df421f01350e6db3c73e61d4b930de8
Josh Oldenburg <josh.oldenburg@me.com>
2019-04-22 20:22:07 -0400

    Add migration for vendor and related models

---
+++ database/migrations/2019_04_22_195526_create_models.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('nationality');
+            $table->string('billing_address');
+            $table->integer('gt_vendor_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('sales_contact')->nullable();
+            $table->string('customer')->nullable();
+            $table->boolean('web_account_exists');
+            $table->string('website')->nullable();
+            $table->string('part_url_schema')->nullable();
+            $table->boolean('shipping_quote_required');
+            $table->boolean('tax_exempt');
+            $table->string('requisition_guidance');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+        Schema::create('vendor_note', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('vendor_id');
+            $table->unsignedInteger('user_id');
+            $table->string('note');
+            $table->unsignedInteger('parent')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('vendor_id')->references('id')->on('vendor');
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('parent')->references('id')->on('vendor_note');
+        });
+        Schema::create('vendor_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('vendor_id');
+            $table->string('tag');
+
+            $table->foreign('vendor_id')->references('id')->on('vendor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendor');
+        Schema::dropIfExists('vendor_note');
+        Schema::dropIfExists('vendor_tag');
+    }
+}
