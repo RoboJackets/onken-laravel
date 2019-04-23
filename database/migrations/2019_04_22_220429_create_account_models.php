03ef1dd7760a48c706fcb00f734577fb3e2a624d
Josh Oldenburg <josh.oldenburg@me.com>
2019-04-22 22:23:42 -0400

    Add account models

---
+++ database/migrations/2019_04_22_220429_create_account_models.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fiscal_years', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('name');
+            $table->boolean('active');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('fiscal_year_id');
+            $table->string('workday_number')->nullable();
+            $table->string('sga_bill_number')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('fiscal_year_id')->references('id')->on('fiscal_years');
+        });
+        Schema::create('account_lines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('account_id');
+            $table->decimal('amount', 8, 2);
+            $table->unsignedInteger('approver_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('approver_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_lines');
+        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('fiscal_years');
+    }
+}
